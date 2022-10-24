034542aede576bb5e4b3d35861f0ec8a5dd4ae37
charavant <charavant@gmail.com>
2022-10-24 22:19:19 +0300

    starting to work

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import json
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('Ship Resource Logistics.csv')
+#print(df)
+Array2d_result = df.to_numpy()
+#print(Array2d_result)
+#print(Array2d_result[:,0])    
 
 
-# Opening JSON file
-
-u = open('UNIT_DEFINITIONS.json')
-
-units = json.load(u)
-
-for i in units['emp_details']:
-    print(i)
-
-
-u.close()
